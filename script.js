571501b48e0ecbce1bb5ab653cc8a157473f8212
Nestor Orozco <nestoreo@MacBook-Air.attlocal.net>
2020-06-16 14:54:21 -0700

    post comment

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-classnames={}
+const allComms = document.getElementById("comments")
+
+function newComment() {
+    const message = document.getElementById("message").value
+    if (message.replace(/ /g, '')=="")
+    {
+      return;
+    }
+    const comment = document.createTextNode(message)
+
+    //creating the todo item in our unorderedlist
+    const comments = document.createElement("li")
+    comments.appendChild(comment)
+
+    //creates todo text
+    allComms.appendChild(comments)
+
+    document.getElementById("message").value=""
+}
